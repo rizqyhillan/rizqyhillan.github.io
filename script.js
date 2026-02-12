ca429aedd3fd21c3ab776d51150501ff10a5eae6
Arthur Cimory <rizqycoe@gmail.com>
2026-02-12 19:54:51 +0700

    1.4

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const sections = document.querySelectorAll('section');
-    const ws = document.querySelectorAll('.ws');
+  const sections = document.querySelectorAll('section');
+    const dots = document.querySelectorAll('.dot');
 
     window.addEventListener('scroll', () => {
       let index = 0;
@@ -9,6 +9,6 @@ const sections = document.querySelectorAll('section');
         if (scrollY >= top) index = i;
       });
 
-      ws.forEach(w => w.classList.remove('active'));
-      if (ws[index]) ws[index].classList.add('active');
+      dots.forEach(dot => dot.classList.remove('active'));
+      if (dots[index]) dots[index].classList.add('active');
     });
