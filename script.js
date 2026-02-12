62efc3e9571159a6f22f081f4ed63d4944f52a31
Arthur Cimory <rizqycoe@gmail.com>
2026-02-12 20:09:34 +0700

    1.6

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function showSection(id) {
-      const sections = document.querySelectorAll('.section');
-      sections.forEach(sec => sec.classList.remove('active'));
-      document.getElementById(id).classList.add('active');
-    }
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(sec => sec.classList.remove('active'));
+    document.getElementById(id).classList.add('active');
+}
